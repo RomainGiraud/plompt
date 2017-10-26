a3aa17a9910585ca2b3e76acf6d9965fbb2cbfef
Romain Giraud <giraud.romain@gmail.com>
2017-10-26 18:22:49 +0200

    Change some special characters

--- src/special_characters.cpp
+++ src/special_characters.cpp
@@ -1,10 +1,10 @@
 #include <special_characters.h>
 
-const char* c_mark = "\u2713";
-const char* x_mark = "\u2717";
+const char* c_mark = "\uf00c";
+const char* x_mark = "\ue009";
 const char* separator = "\uE0B0";
 const char* separator_thin = "\uE0B1";
 const char* lock = "\uE0A2";
 const char* separator2 = "\u25B6";
-const char* separator_thin2 = "\u276F";
+const char* separator_thin2 = "\uf054";
 const char* ellipsis = "\u2026";
